03ddf64cebf5ae57624354160b172c4fcf82535e
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-09-13 21:25:06 -0300

    Update to jnativehook library

--- src/Main.java
+++ src/Main.java
@@ -8,41 +8,42 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.InputEvent;
+import java.lang.annotation.Native;
 
-public class Main {
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+
+public class Main implements NativeKeyListener {
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        if (e.getKeyCode() == NativeKeyEvent.VC_B) {
+            System.out.println("Break");
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException nativeHookException) {
+                nativeHookException.printStackTrace();
+            }
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println(e.getKeyChar());
+    }
+
+
     public static void main(String[] args) {
         MyLabel myLabel = new MyLabel();
         MyFrame myFrame = new MyFrame(myLabel);
 
-        StartClicker startClicker = new StartClicker();
-
-        myFrame.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                if (e.getKeyChar() == KeyEvent.VK_B) {
-                    try {
-                        startClicker.clicker();
-                    } catch (AWTException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyChar() == KeyEvent.VK_A) {
-                    try {
-                        startClicker.clicker();
-                    } catch (AWTException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
-        });
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.out.println(ex.getMessage());
+        }
+        GlobalScreen.addNativeKeyListener(new Main());
     }
 }
--- src/org/william/StartClicker.java
+++ src/org/william/StartClicker.java
@@ -2,19 +2,23 @@ package org.william;
 
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class StartClicker {
-    public void clicker() throws AWTException {
-        try {
-            Robot robot = new Robot();
-            robot.delay(2000);
-            for (int i = 0; i <= 15; i ++) {
-                robot.delay(50);
-                robot.mousePress(InputEvent.getMaskForButton(1));
-                robot.mouseRelease(InputEvent.getMaskForButton(1));
+    public void clicker(int state) throws AWTException {
+            try {
+                Robot robot = new Robot();
+                while (state != 2) {
+                    robot.delay(50);
+//                    robot.mousePress(InputEvent.getMaskForButton(1));
+//                    robot.mouseRelease(InputEvent.getMaskForButton(1));
+                    robot.keyPress(KeyEvent.VK_L);
+                    if (state == 2) {
+                        break;
+                    }
+                }
+            } catch (AWTException e) {
+                e.printStackTrace();
             }
-        } catch (AWTException e) {
-            e.printStackTrace();
-        };
     }
 }
