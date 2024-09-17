f8367947fe4f65ba6b515822ca7a3bcfd9560afd
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-09-17 20:19:40 -0300

    Add JMenu

--- src/org/william/frame/MyFrame.java
+++ src/org/william/frame/MyFrame.java
@@ -1,16 +1,46 @@
 package org.william.frame;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
+    JButton jButton;
+
     public MyFrame(Component addComponent) {
+        jButton = new JButton();
+        jButton.setBounds(100, 50, 120, 40);
+        jButton.addActionListener(this);
+        jButton.setText("Is Actived");
+        jButton.setFocusable(false);
+        jButton.setBackground(new Color(0xE8E8E8));
+        jButton.setFont(new Font("Helvetica", Font.BOLD, 10));
+        jButton.setBorder(BorderFactory.createLineBorder(new Color(0x151515)));
+
+        JMenu jMenu = new JMenu("Options");
+        JMenuItem mI = new JMenuItem("1");
+        JMenuBar jMenuBar = new JMenuBar();
+
+        jMenu.add(mI);
+        jMenuBar.add(jMenu);
+
         this.setTitle("Auto Clicker");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.setResizable(false);
         this.setSize(300, 200);
         this.add(addComponent);
-        this.setFocusable(true);
+        this.setJMenuBar(jMenuBar);
+        this.add(jButton);
+        this.setLayout(null);
+        this.setResizable(true);
+        //this.setFocusable(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == jButton) {
+
+        }
     }
 }
--- src/org/william/label/MyLabel.java
+++ src/org/william/label/MyLabel.java
@@ -6,5 +6,6 @@ import java.awt.*;
 public class MyLabel extends JLabel {
     public MyLabel() {
         this.setBackground(new Color(0xE8E8E8));
+        //this.setHorizontalAlignment(JLabel.CENTER);
     }
 }
